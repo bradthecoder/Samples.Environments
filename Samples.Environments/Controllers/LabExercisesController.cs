d01c3b5c2a8618464039dfbf49a32ab71f311ff7
Brad Huggins <bradthecoder@outlook.com>
2021-07-22 19:41:04 -0500

    completing lab exercise

--- Samples.Environments/Controllers/LabExercisesController.cs
+++ Samples.Environments/Controllers/LabExercisesController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Samples.Environments.Models;
 
@@ -7,16 +9,38 @@ namespace Samples.Environments.Controllers
     public class LabExercisesController : Controller
     {
         private readonly ILogger<LabExercisesController> _logger;
+        private readonly IWebHostEnvironment _environment;
 
-        public LabExercisesController(ILogger<LabExercisesController> logger)
+        public LabExercisesController(ILogger<LabExercisesController> logger, IWebHostEnvironment environment)
         {
             _logger = logger;
+            _environment = environment;
         }
+
         public IActionResult AccessInController()
         {
             LabExercisesViewModel model = new LabExercisesViewModel();
             //hydrate model here
-
+            if (_environment.IsDevelopment())
+            {
+                model.Message += "Warning, you are in DEVELOPMENT!";
+            }
+            else if (_environment.IsStaging())
+            {
+                model.Message = "Caution, you are in STAGING!";
+            }
+            else if (_environment.IsEnvironment("Staging_2"))
+            {
+                model.Message = "Caution, you are in STAGING #2!";
+            }
+            else if (_environment.IsProduction())
+            {
+                model.Message += "Welcome to PRODUCTION!";
+            }
+            else
+            {
+                model.Message = "Error! Unknown ";
+            }
             return View(model);
         }
         public IActionResult TagHelper()
